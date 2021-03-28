602d8169df9839940c75522789e00a57483aad9c
Alexander Plotnikov <lotos_toi@mail.ru>
2021-03-28 20:28:05 +1000

    is ready

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import CounterClass from './counters/class.js'
-import CounterFunction from './counters/function.js'
+import Valid from './counters/function.js'
 
 export default function() {
   return (
     <div>
       <h2>Counter as function</h2>
-      <CounterFunction min={0} max={25} />
+      <Valid min={0} max={25} />
     </div>
   )
 }
